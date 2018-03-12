d8aaed5fadf5a6bc5efba695c8ae327f59455b06
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-03-12 11:33:19 +0100

    added hash utility (sha-256)

---
+++ handler/Utility.java
@@ -0,0 +1,35 @@
+package handler;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Utility {
+    public static String hashSHA256(String input){
+        String hash = "";
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] h = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            hash = toHexString(h);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return hash;
+    }
+
+    public static String toHexString(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+}
