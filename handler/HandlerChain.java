3eb8fe2536550043259a29578358edf9b3ac1f03
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-03-21 08:06:25 +0100

    changed to for loop to propagate exception

--- handler/HandlerChain.java
+++ handler/HandlerChain.java
@@ -18,19 +18,12 @@ public class HandlerChain<T> implements Handler<T> {
 
     @Override
     public T handle(Command command) throws RequestMalformedException{
-        //TODO Exception not compatible with stream format change to regular for loop
-
-        return this.handlers.stream()
-                .map(x -> {
-                    try {
-                        return x.handle(command);
-                    } catch (RequestMalformedException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                })
-                .filter( x -> x != null)
-                .findFirst()
-                .orElse(null);
+        T result = null;
+        for (Handler<T> h : this.handlers){
+            result = h.handle(command);
+            if (result != null)
+                return result;
+        }
+        return null;
     }
 }
