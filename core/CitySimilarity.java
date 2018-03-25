720cc9b171917b4670b845ae823bb1566ea7bace
ben <bencrulis@gmail.com>
2018-03-25 15:11:14 +0200

    created commune distance calculator

---
+++ core/CitySimilarity.java
@@ -0,0 +1,18 @@
+package core;
+
+import beans.Commune;
+
+public class CitySimilarity {
+
+    public CitySimilarity(){
+
+    }
+
+
+    public Double calculateDistance(Commune commune1, Commune commune2){
+
+
+        return 1.0;
+    }
+
+}
