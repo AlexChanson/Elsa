1e9286691617693013f8d3fb3565df54460d517b
Ben Crulis <bencrulis@gmail.com>
2018-03-28 20:33:38 +0200

    Fixed divisions by 0, make subMetric equal to 1 when both divisors are 0

--- core/CitySimilarity.java
+++ core/CitySimilarity.java
@@ -11,24 +11,27 @@ public class CitySimilarity {
 
     public Double calculateDistance(Commune commune1, Commune commune2){
 
-        double actifsDist = Math.pow((commune1.getActifs_2010()-commune2.getActifs_2010())/
-                ((double)Math.max(commune1.getActifs_2010(), commune2.getActifs_2010())),2.0);
+        double divisor = Math.max(commune1.getActifs_2010(), commune2.getActifs_2010());
+        double actifsDist = divisor == 0? 1 : Math.pow((commune1.getActifs_2010()-commune2.getActifs_2010())/
+                divisor,2.0);
 
-        double actifs2015Dist = Math.pow((commune1.getNb_actifs_2015()-commune2.getNb_actifs_2015())/(double)
-                Math.max(commune1.getNb_actifs_2015(), commune2.getNb_actifs_2015()),2);
+        divisor = Math.max(commune1.getNb_actifs_2015(), commune2.getNb_actifs_2015());
+        double actifs2015Dist = divisor == 0? 1 : Math.pow((commune1.getNb_actifs_2015()-commune2.getNb_actifs_2015())/divisor,2);
 
-        double publicEtDist = Math.pow((commune1.getNb_etablissements()-commune2.getNb_etablissements())/(double)
-                Math.max(commune1.getNb_etablissements(), commune2.getNb_etablissements()),2);
+        divisor = Math.max(commune1.getNb_etablissements(), commune2.getNb_etablissements());
+        double publicEtDist = divisor == 0? 1 : Math.pow((commune1.getNb_etablissements()-commune2.getNb_etablissements())/
+                divisor,2);
 
-        double nbEtuDist = Math.pow((commune1.getNb_etudiants()-commune2.getNb_etudiants())/(double)
-                Math.max(commune1.getNb_etudiants(), commune2.getNb_etudiants()),2);
+        divisor = Math.max(commune1.getNb_etudiants(), commune2.getNb_etudiants());
+        double nbEtuDist = divisor == 0? 1 : Math.pow((commune1.getNb_etudiants()-commune2.getNb_etudiants())/divisor,2);
 
         double evPopDist = commune1.getEvolution_pop().equals(commune2.getEvolution_pop())? 0.8 : 1.0;
 
         double fideliteDist = commune1.getFidelite().equals(commune2.getFidelite())? 0.8 : 1.0;
 
-        double urbaniteDist = Math.pow((commune1.getScore_urbanite()-commune2.getScore_urbanite())/
-                Math.max(commune1.getScore_urbanite(), commune2.getScore_urbanite()),2);
+        divisor = Math.max(commune1.getScore_urbanite(), commune2.getScore_urbanite());
+        double urbaniteDist = divisor == 0? 1 : Math.pow((commune1.getScore_urbanite()-commune2.getScore_urbanite())/
+                divisor,2);
 
         /*
         System.out.println(actifsDist);
