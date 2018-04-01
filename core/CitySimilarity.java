42025aab5d0dae0a2a59eb0b39ed28a82ad6fb0a
Ben Crulis <bencrulis@gmail.com>
2018-04-01 13:25:02 +0200

    fixed metric, removed unused classes

--- core/CitySimilarity.java
+++ core/CitySimilarity.java
@@ -12,25 +12,25 @@ public class CitySimilarity {
     public Double calculateDistance(Commune commune1, Commune commune2){
 
         double divisor = Math.max(commune1.getActifs_2010(), commune2.getActifs_2010());
-        double actifsDist = divisor == 0? 1 : Math.pow((commune1.getActifs_2010()-commune2.getActifs_2010())/
+        double actifsDist = divisor == 0? 0 : Math.pow((commune1.getActifs_2010()-commune2.getActifs_2010())/
                 divisor,2.0);
 
         divisor = Math.max(commune1.getNb_actifs_2015(), commune2.getNb_actifs_2015());
-        double actifs2015Dist = divisor == 0? 1 : Math.pow((commune1.getNb_actifs_2015()-commune2.getNb_actifs_2015())/divisor,2);
+        double actifs2015Dist = divisor == 0? 0 : Math.pow((commune1.getNb_actifs_2015()-commune2.getNb_actifs_2015())/divisor,2);
 
         divisor = Math.max(commune1.getNb_etablissements(), commune2.getNb_etablissements());
-        double publicEtDist = divisor == 0? 1 : Math.pow((commune1.getNb_etablissements()-commune2.getNb_etablissements())/
+        double publicEtDist = divisor == 0? 0 : Math.pow((commune1.getNb_etablissements()-commune2.getNb_etablissements())/
                 divisor,2);
 
         divisor = Math.max(commune1.getNb_etudiants(), commune2.getNb_etudiants());
-        double nbEtuDist = divisor == 0? 1 : Math.pow((commune1.getNb_etudiants()-commune2.getNb_etudiants())/divisor,2);
+        double nbEtuDist = divisor == 0? 0 : Math.pow((commune1.getNb_etudiants()-commune2.getNb_etudiants())/divisor,2);
 
         double evPopDist = commune1.getEvolution_pop().equals(commune2.getEvolution_pop())? 0.8 : 1.0;
 
         double fideliteDist = commune1.getFidelite().equals(commune2.getFidelite())? 0.8 : 1.0;
 
         divisor = Math.max(commune1.getScore_urbanite(), commune2.getScore_urbanite());
-        double urbaniteDist = divisor == 0? 1 : Math.pow((commune1.getScore_urbanite()-commune2.getScore_urbanite())/
+        double urbaniteDist = divisor == 0? 0 : Math.pow((commune1.getScore_urbanite()-commune2.getScore_urbanite())/
                 divisor,2);
 
         /*
