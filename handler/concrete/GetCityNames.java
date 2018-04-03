7cae9bb3033576b2b2c66565eb6b67ccb61099d8
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-04-03 18:36:22 +0200

    memory efficient conversion

--- handler/concrete/GetCityNames.java
+++ handler/concrete/GetCityNames.java
@@ -1,31 +1,40 @@
 package handler.concrete;
 
-import beans.Commune;
+
 import beans.CommuneSimple;
+import com.google.gson.stream.JsonWriter;
 import dao.BasicVirtualTable;
 import handler.Command;
 import handler.Handler;
 import handler.RequestResult;
 import handler.Utility;
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.OutputStreamWriter;
+
 
 public class GetCityNames implements Handler<RequestResult>{
     @Override
     public RequestResult handle(Command command) {
-        if (checkType(command, "getCityNames"))
-            return process();
+        if (checkType(command, "getCityNames")) {
+            try {
+                return process();
+            } catch (IOException e) {
+                return null;
+            }
+        }
         return null;
     }
 
-    private RequestResult process() {
+    private RequestResult process() throws IOException {
 
         BasicVirtualTable<CommuneSimple> bvt = new BasicVirtualTable<>(CommuneSimple.class);
-        List<CommuneSimple> communeSimples = bvt.getAll();
-        String buffer = Utility.gson.toJson(communeSimples);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
+        writer.beginArray();
+        bvt.getStream().forEach(comm -> Utility.gson.toJson(comm, CommuneSimple.class, writer));
+        writer.endArray();
+        String buffer = new String(out.toByteArray());
         return new RequestResult() {
             @Override
             public String toJson() {
