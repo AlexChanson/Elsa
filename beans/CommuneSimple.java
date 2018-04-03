1f238d0ac9a2bb74041336f4aa560e87b65c57e7
Ben Crulis <bencrulis@gmail.com>
2018-04-03 18:30:26 +0200

    optimised getCityNames handling of CommuneSimple results

--- beans/CommuneSimple.java
+++ beans/CommuneSimple.java
@@ -1,8 +1,17 @@
 package beans;
 
-public class CommuneSimple {
-    private String code_insee, nom, num_dep;
+import dao.DaoConstructor;
+import dao.Entity;
+import dao.Key;
 
+@Entity(tableName = "CommuneSimple")
+public class CommuneSimple {
+    @Key
+    private final String code_insee;
+    private final String nom;
+    private final String num_dep;
+
+    @DaoConstructor
     public CommuneSimple(String code_insee, String nom, String num_dep) {
         this.code_insee = code_insee;
         this.nom = nom;
